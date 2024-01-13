b5c02834db9c73a4ced62ff97cc315a677b2f4f0
nakagame <yutonakagame@gmail.com>
2024-01-13 11:42:07 +0900

    Angular FrontEnd Create

---
+++ src/app/product/products.ts
@@ -0,0 +1,57 @@
+import { getLocaleEraNames } from "@angular/common";
+
+export const products = [
+    {
+        coverImage: '../assets/img/phone-cover.jpg',
+        name: 'Phone XL',
+        price: 799,
+        stock: 50,
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse ducimus cupiditate, distinctio non iste numquam assumenda adipisci soluta quam culpa!',
+        heading1: 'Hello!',
+        heading2: 'Hello',
+        heading3: 'Hell',
+        headingtext1: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+        headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consecte',
+        headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+    },
+    {
+        coverImage: '../assets/img/phone-cover.jpg',
+        name: 'Phone Mini',
+        price: 699,
+        stock: 45,
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse ducimus cupiditate, distinctio non iste numquam assumenda adipisci soluta quam culpa!',
+        heading1: 'Hello!',
+        heading2: 'Hello',
+        heading3: 'Hell',
+        headingtext1: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+        headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consecte',
+        headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+    },
+    {
+        coverImage: '../assets/img/phone-cover.jpg',
+        name: 'Phone Standard',
+        price: 299,
+        stock: 30,
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse ducimus cupiditate, distinctio non iste numquam assumenda adipisci soluta quam culpa!',
+        heading1: 'Hello!',
+        heading2: 'Hello',
+        heading3: 'Hell',
+        headingtext1: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+        headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consecte',
+        headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+    },
+    {
+        coverImage: '../assets/img/phone-cover.jpg',
+        name: 'Phone Spacial',
+        price: 999,
+        stock: 5,
+        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse ducimus cupiditate, distinctio non iste numquam assumenda adipisci soluta quam culpa!',
+        heading1: 'Hello!',
+        heading2: 'Hello',
+        heading3: 'Hell',
+        headingtext1: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+        headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consecte',
+        headingtext3: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.'
+    },
+];
+
